55424006be0354c838b5f0854bcd4a52d528ff5f
Lloyd McKenzie <lloyd@lmckenzie.com>
2020-07-20 14:08:44 -0600

    Added additional diagrams and text on exchanging

---
+++ gendiagrams.bat
@@ -0,0 +1,3 @@
+rem java -jar plantuml.jar -tsvg -o ../../input/images input/images-source/*.plantuml
+rem java -jar plantuml.jar -tsvg -o "C:\Users\lloyd\Documents\SVN\_IGs\davinci-ehrx/temp\pages\_includes" "C:\Users\lloyd\Documents\SVN\_IGs\davinci-ehrx/input/images-source/*.plantuml"
+java -jar plantuml.jar -tsvg -o "C:\Users\lloyd\Documents\SVN\_IGs\davinci-ehrx/input/includes" "C:\Users\lloyd\Documents\SVN\_IGs\davinci-ehrx/input/images-source/*.plantuml"
